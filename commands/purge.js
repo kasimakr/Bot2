962ef3fcb68296631807b78c3b461d4b22530b48
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-08 19:20:41 -0500

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const sendError = require("../util/error");
+const sendTPemplate = require("../util/mputemplate");
+const sendTemplate = require("../util/mptemplate");
+
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) //Here we check if the user can run the command.
+    return sendError("You do not have the nessecary permissions to purge.", message.channel, message.react('759498707774734407'));
+
+  const deleteCount = parseInt(args[0], 10); //This will get the number of messages we want to delete as an integer.
+  if (!deleteCount || deleteCount < 0 || deleteCount > 10000000000) //This makes sure that the minimum amount of messages we can delete is 2, and the max is 100. You can change this if you want.
+    return sendTPemplate("Please specify a amount you would like to delete.", message.channel, message.react('759498707774734407'));
+  message.channel
+    .bulkDelete(deleteCount) //This will delete the specified number of messages.
+    .catch(error =>
+      message.channel.sendError(`Couldn't purge messages because of, ${error}.`) //This will make the bot send a message if there is an error.
+    );
+};
+
+module.exports.help = {
+  name: "purge"
+};
