58677c26e3e51df7ac36a0626628ce8f957b99c8
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-26 17:51:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({
+onst Discord = require("discord.js");
+
+const client = new Discord.Client({
 	partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
-const botconfig = require("./botconfig.json");
 const fs = require("fs");
-bot.commands = new Discord.Collection();
+client.commands = new Discord.Collection();
+
+client.db = require("quick.db");
+client.commands = new Discord.Collection();
+client.cooldown = new Discord.Collection();
+client.config = {
+    TOKEN: "NzMzMTQ5ODE0MTc0MjUzMTE2.Xw-8-A.wiEtFZNm3T6JfgYZgSPzJvxPShU",
+    prefix: ";",
+    cooldown: 15000
+};
 
 fs.readdir("./commands/", (err, files) => {
 	if (err) console.log(err);
@@ -16,23 +25,45 @@ fs.readdir("./commands/", (err, files) => {
 	jsfile.forEach((f, i) => {
 		let props = require(`./commands/${f}`);
 		console.log(`${f} loaded.`);
-		bot.commands.set(props.help.name, props);
+		client.commands.set(props.help.name, props);
 	});
 });
 
-
-bot.on("message", async message => {
-	if (!message.content.startsWith(botconfig.prefix) || message.author.bot)
-		return;
-	if (message.channel.type === "dm") return;
-
-	let prefix = botconfig.prefix;
-	let messageArray = message.content.split(" ");
-	let cmd = messageArray[0];
-	let args = messageArray.slice(1);
-
-	let commandfile = bot.commands.get(cmd.slice(prefix.length));
-	if (commandfile) commandfile.run(bot, message, args);
+// Events
+client.once("ready", () => {
+    console.log("Ready!");
 });
 
-bot.login('AYE');
+client.on("error", console.error);
+
+client.on("warn", console.warn);
+
+client.on("message", async (message) => {
+    if (!message.guild || message.author.bot) return;
+    // Handle XP
+    xp(message);
+    // command handler
+    if (!message.content.startsWith(client.config.prefix)) return;
+    let args = message.content.slice(client.config.prefix.length).trim().split(" ");
+    let command = args.shift().toLowerCase();
+    let commandFile = client.commands.get(command);
+    if (!commandFile) return;
+    commandFile.run(client, message, args);
+});
+
+function xp(message) {
+    if (!client.cooldown.has(`${message.author.id}`) || !(Date.now() - client.cooldown.get(`${message.author.id}`) > client.config.cooldown)) {
+        let xp = client.db.add(`xp_${message.author.id}`, 1);
+        let level = Math.floor(0.3 * Math.sqrt(xp));
+        let lvl = client.db.get(`level_${message.author.id}`) || client.db.set(`level_${message.author.id}`,1);;
+        if (level > lvl) {
+            let newLevel = client.db.set(`level_${message.author.id}`,level);
+            message.channel.send(` ${message.author.toString()}, You just advanced to level ${newLevel}! Lets celebrate :tada: `);
+        }
+        client.cooldown.set(`${message.author.id}`, Date.now());
+    }
+}
+
+
+
+client.login('NzMzMTQ5ODE0MTc0MjUzMTE2.Xw-8-A.wiEtFZNm3T6JfgYZgSPzJvxPShU');
