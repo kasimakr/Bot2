9fd31e05f97a8a9895d4ed4c93d824360bbbf09a
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-26 17:45:55 -0500

    Create announcement.js

---
+++ commands/announcement.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "announce",
+  run: async (bot, message, args) => {
+    let rChannel = message.guild.channels.cache.get(args[0]);
+    if (!rChannel)
+      return message.channel.send(
+        `You did not specify your channel to send the announcement too!`
+      );
+    console.log(rChannel);
+    let MSG = message.content
+      .split(`${bot.prefix}announce ${rChannel.id} `)
+      .join("");
+    if (!MSG)
+      return message.channel.send(`You did not specify your message to send!`);
+    let Reason = MSG.replace(';announce',' ');
+    const _ = new MessageEmbed()
+    
+      .setTitle(`New announcement!`)
+      .setDescription(`${Reason}`)
+      .setColor("RANDOM");
+    rChannel.send(_);
+    message.delete();
+  },
+};
+
+module.exports.help = {
+    name: "announce"
+};
