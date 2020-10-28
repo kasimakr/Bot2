4aab4931d7f36118ea54d237abbd6be6089d913b
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-28 13:56:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-const { MessageEmbed } = require("discord.js");
-const sendError = require("../util/error");
 
-module.exports = {
-  run: async (bot, message, args) => {
-    if (!message.member.permissions.has("MANAGE_MESSAGES"))
-      return sendError("You do not have the nessecary permissions to report.", message.channel, message.react('759498707774734407'));
-    let User = message.mentions.users.first() || null;
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
 
-    if (User == null) {
-      return sendError("Please mention a person. Example ;report <@!733149814174253116> Being Mean", message.channel, message.react('759498707774734407'));
-    } else {
-      let Reason = message.content.slice(";" + 22 + 7) || null;
-      if (Reason == null) {
-        return sendError("Please give a reason to report", message.channel, message.react('759498707774734407'));
-      }
-      let Avatar = User.displayAvatarURL();
-      let Channel = message.guild.channels.cache.find(
-        (ch) => ch.name === "reports"
-      );
-      if (!Channel)
-        return sendError("Please create a channel called **reports** to send all reports in.", message.channel, message.react('759498707774734407'));
-      let Embed = new MessageEmbed()
-        .setAuthor("Report", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
-        .setDescription(
-          `<@!${message.author.id}> reported <@!${User.id}>. `
-        )
-        .setColor(`RED`)
-        .setThumbnail(Avatar) 
-       .setTimestamp()
-       .setFooter("Start a support ticket if you have any issues!")
-        .addFields(
-          { name: "Reason", value: `${Reason.slice(1)}`, inline: true },
-          {
-            name: "Date of report",
-            value: `${new Intl.DateTimeFormat("en-US").format(Date.now())}`,
-            inline: true,
-          }
-        );
-      Channel.send(Embed);
+client.db = require("quick.db");
+client.commands = new Discord.Collection();
+client.cooldown = new Discord.Collection();
+client.config = {
+    TOKEN: "NzMzMTQgKwfOcKPsC2FSfX4",
+    prefix: ";",
+    cooldown: 15000
+};
+
+// Load Commands
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        let command = require(`./commands/${f}`);
+        client.commands.set(command.help.name, command);
+    });
+});
+
+// Events
+client.once("ready", () => {
+    console.log("Ready!");
+});
+
+client.on("error", console.error);
+
+client.on("warn", console.warn);
+
+client.on("message", async (message) => {
+    if (!message.guild || message.author.bot) return;
+    // Handle XP
+    xp(message);
+    // command handler
+    if (!message.content.startsWith(client.config.prefix)) return;
+    let args = message.content.slice(client.config.prefix.length).trim().split(" ");
+    let command = args.shift().toLowerCase();
+    let commandFile = client.commands.get(command);
+    if (!commandFile) return;
+    commandFile.run(client, message, args);
+});
+
+function xp(message) {
+    if (!client.cooldown.has(`${message.author.id}`) || !(Date.now() - client.cooldown.get(`${message.author.id}`) > client.config.cooldown)) {
+        let xp = client.db.add(`xp_${message.author.id}`, 1);
+        let level = Math.floor(0.3 * Math.sqrt(xp));
+        let lvl = client.db.get(`level_${message.author.id}`) || client.db.set(`level_${message.author.id}`,1);;
+        if (level > lvl) {
+            let newLevel = client.db.set(`level_${message.author.id}`,level);
+            message.channel.send(`${message.author.toString()}, You just leveled up to level ${newLevel}!`);
+        }
+        client.cooldown.set(`${message.author.id}`, Date.now());
     }
-  },
-};
+}
 
-module.exports.help = {
-  name: "report"
-};
+client.login(client.config.TOKEN);
+client.login('NzX4');
