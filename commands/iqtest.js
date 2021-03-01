3856fbb6f91127add089e542a5c18a27c56fcec1
kasimakr <64462692+kasimakr@users.noreply.github.com>
2021-02-28 19:54:00 -0600

    Create iqtest.js

---
+++ commands/iqtest.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "iqtest",
+    callback: async (message) => {
+        const iq = Math.floor(Math.random() * 100) + 1;
+
+        const iEmbed = new Discord.MessageEmbed()
+            .setTitle("IQ Test")
+            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setDescription(`Your IQ is: \`${iq}\`!`)
+
+        const ioEmbed = new Discord.MessageEmbed()
+            .setTitle("IQ Test")
+            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setDescription(`Your IQ is: \`1,000,000,000,000\`!`)
+
+        if (message.author.id !== message.guild.owner.id) {
+        message.channel.send(iEmbed);
+        } else {
+          message.channel.send(ioEmbed);
+        };
+    },
+};
+module.exports.help = {
+    name: "iqtest"
+};
