142be0a400e0c709dbb237ec381bad5bfc92c930
kasimakr <64462692+kasimakr@users.noreply.github.com>
2021-03-01 18:54:06 -0600

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,6 +1,6 @@
 const {Client, Message, MessageEmbed} = require('discord.js');
 module.exports = {
-    name:'kill',
+    name: "kill",
     run:async(client,message,args)=>{
         const target = message.mentions.users.first()
         const DyingWays = ['Scooter ankle R.I.P','Putting a toaster in the bath','promising ur son robux','insulting gurenge that idiot','watching gacha','watching a netflix adaptation','insulting e']
